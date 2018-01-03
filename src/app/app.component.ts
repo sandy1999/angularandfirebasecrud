519db2b3bbcd8ffeae823164d507c0e1cc07db01
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-03 23:54:19 +0530

    applied ngOnDestroyed method for prevention of memory leaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy  {
   title = 'app';
+  subscription:Subscription;
+  courses:any[];
+  constructor(private db:AngularFireDatabase){
+    this.subscription = db.list('/courses').valueChanges().subscribe(courses=>{
+      this.courses = courses;
+      console.log(this.courses);
+    })
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
