6242f809a8ffd7553f2959c723f1b5ada09ff265
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-03 23:17:55 +0530

    sucessfully installed angularfire2 and firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDFjJzGjKqbIWq9mGf88H5BY7RA1gS8mx0",
+    authDomain: "basicdemo-c6d18.firebaseapp.com",
+    databaseURL: "https://basicdemo-c6d18.firebaseio.com",
+    projectId: "basicdemo-c6d18",
+    storageBucket: "basicdemo-c6d18.appspot.com",
+    messagingSenderId: "38120782443"
+  }
 };
