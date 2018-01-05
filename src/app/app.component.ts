d8b6afa31f7eb53eb81fd691fc3592dafd1e16cc
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-05 15:29:38 +0530

    reterival of data using async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { FirebaseDatabase } from '@firebase/database-types';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 @Component({
@@ -7,18 +9,12 @@ import { Subscription } from 'rxjs/Subscription';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy  {
+export class AppComponent {
   title = 'app';
   subscription:Subscription;
   courses:any[];
+  courses$;
   constructor(private db:AngularFireDatabase){
-    this.subscription = db.list('/courses').valueChanges().subscribe(courses=>{
-      this.courses = courses;
-      console.log(this.courses);
-    })
-  }
-
-  ngOnDestroy(){
-    this.subscription.unsubscribe();
+    this.courses$ = db.list('courses').valueChanges();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+// import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
@@ -14,7 +15,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    // AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
